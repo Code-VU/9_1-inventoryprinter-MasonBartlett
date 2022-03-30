cb80044b9d00326ae7666b58dc812e777a30d7ee
Mason Bartlett <Mason.Bartlett@vu.com>
2022-03-30 17:27:40 -0500

    first try

--- displayInventory.py
+++ displayInventory.py
@@ -1,6 +1,10 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}       
 
 def displayInventory(inventory):
+    for keys,values in stuff.items():
+        print (keys, ':', values)
+
+
     # your code goes here
 
-##displayInventory(stuff)
+displayInventory(stuff)
